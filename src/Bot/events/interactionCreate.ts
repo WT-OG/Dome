b3fff87b760cf7164bf27ceb5fbe50414ae7d565
WT-OG <kbrowloski@gmail.com>
2024-04-03 00:24:22 -0400

    Ok Discord Bot stuff is finally finished chat, im cooking

--- src/Bot/events/interactionCreate.ts
+++ src/Bot/events/interactionCreate.ts
@@ -1,5 +1,5 @@
 import { CommandInteractionOptionResolver } from "discord.js";
-import { client } from "..";
+import { client } from "../..";
 import { Event } from "../structures/Event";
 import { ExtendedInteraction } from "../typings/Command";
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 require("dotenv").config();
-import { ExtendedClient } from "./structures/Client";
+import { ExtendedClient } from "./Bot/structures/Client";
 
 export const client = new ExtendedClient();
 
 client.start();
+
+const express = require("express");
+const Express = express();
+const fs = require("fs");
+const path = require("path")
+const cookiepars = require("cookie-parser");
